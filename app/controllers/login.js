176f5fedfe33e019fd604ca2ca799baa11ab52bd
Leonardo Everson <llinharespinheiro@gmail.com>
2018-07-05 18:38:20 -0300

    ajustes de elementos da interface

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,7 +6,6 @@ module.exports.login_usuario = function(app, request, response){
 
 	loginUsuario.valida_login(body, function(error, result){
 		if(!error && result.length > 0){
-			console.log(result[0].senha)
 			bcrypt.compare(body.senha, result[0].senha, function(err, res) {
 			    if(res == true){
 			    	response.redirect("/home");
@@ -17,11 +16,13 @@ module.exports.login_usuario = function(app, request, response){
 			
 		}else{
 			if(error){
-				console.log(error);
+				response.render("login/index",{validacao : [{'msg':'error'}]})
+				return
 			}
 
 			if(result.length == 0){
 				response.render("login/index",{validacao : [{'msg':'usuário não encontrado'}]})
+				return
 			}
 		}
 	})
--- app/routes/home.js
+++ app/routes/home.js
@@ -2,4 +2,8 @@ module.exports = function(app){
 	app.get('/home',function(request, response){
 		response.render('home/index');
 	})
+
+	app.get('/index',function(request, response){
+		response.render('home/index');
+	})
 }
