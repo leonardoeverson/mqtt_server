1e234d72dc404656ae55bf322e68d0df9135bc57
Leonardo Everson <llinharespinheiro@gmail.com>
2018-07-11 22:08:44 -0300

    cadastro de dispositivos

--- app/controllers/devices.js
+++ app/controllers/devices.js
@@ -1,5 +1,19 @@
 module.exports.list_devices = function(app, request, response){
+	var conn = app.config.dbconn();
+	var dadosDispositivos = new app.app.models.devicesDAO(conn);
 
+	var dados = {};
+	dados.user_id = request.session.user_id;
+	//
+
+	dadosDispositivos.list_devices_db(dados, function(error, result){
+		if(!error){
+			response.render("devices/register",{validacao : result});
+		}else{
+			console.log(error)
+			response.render("devices/register",{validacao : {}});
+		}
+	})
 }
 
 module.exports.register_devices = function(app, request, response){
