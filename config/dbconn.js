241f16ef96b578aced06b66cab2b344ad7a3e880
Leonardo Everson <llinharespinheiro@gmail.com>
2018-09-15 12:59:07 -0300

    Update dbconn.js

--- config/dbconn.js
+++ config/dbconn.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var dbconn = function(){
 		return conn = mysql.createConnection({
-		host: 'cnsede.dyndns.org:20000',
+		host: 'cbsede.dyndns.org:20000',
 		user: 'mqtt',
 		password : 'mqtt_server_01#',
 		database: 'mqtt'});
