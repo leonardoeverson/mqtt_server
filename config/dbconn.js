a18b3ebaa6c8d4ac6b81cccfe94798d1ec968b51
Leonardo Everson <llinharespinheiro@gmail.com>
2018-09-15 18:13:07 -0300

    configuração de banco de dados

--- config/dbconn.js
+++ config/dbconn.js
@@ -2,10 +2,9 @@ var mysql = require('mysql');
 
 var dbconn = function(){
 		return conn = mysql.createConnection({
-		host: 'cbsede.dyndns.org',
-		port:20000,
-		user: 'mqtt',
-		password : 'mqtt_server_01#',
+		host: 'localhost',
+		user: 'root',
+		password : 'db_node_01#',
 		database: 'mqtt'});
 }
 
