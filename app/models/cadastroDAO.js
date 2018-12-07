749ba7b17a25a90c95a5ac93ce159332ab88b977
Leonardo Everson <llinharespinheiro@gmail.com>
2018-12-07 09:30:32 -0300

    configuração de ambiente

--- app/models/cadastroDAO.js
+++ app/models/cadastroDAO.js
@@ -8,12 +8,12 @@ cadastroDAO.prototype.grava_usuario = function(dados, callback) {
 	var salt = bcrypt.genSaltSync(saltRounds);
 	dados.senha = bcrypt.hashSync(dados.senha, salt);
 
-	var query = "insert into usuarios (nome, email, senha) values('"+dados.nome+"','"+dados.email+"','"+dados.senha+"')";
+	var query = "insert into users (nome, email, senha) values('"+dados.nome+"','"+dados.email+"','"+dados.senha+"')";
 	this.connection.query(query, callback);
 };
 
 cadastroDAO.prototype.verifica_email_existente = function(email, callback) {
-	var query = "select email from usuarios where email ='"+email+"'";
+	var query = "select email from users where email = '"+email+"'";
 	this.connection.query(query, callback);
 };
 
--- app/models/dadosUsuarioDAO.js
+++ app/models/dadosUsuarioDAO.js
@@ -4,7 +4,7 @@ function dadosUsuariosDAO(conn){
 
 
 dadosUsuariosDAO.prototype.verifica_dados_usuarios = function(dados, callback) {
-	var query = "SELECT email, senha FROM usuarios where email ='"+dados.email+"'' and senha = '"+dados.senha+"'";
+	var query = "SELECT email, senha FROM users where email ='"+dados.email+"'' and senha = '"+dados.senha+"'";
 	this.connection.query(query, callback);
 };
 
