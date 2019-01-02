d40870c6c4059b2a2093cd631ddb114ab4f698a0
leonardoeverson <llinharespinheiro@gmail.com>
2019-01-02 06:31:33 -0300

    ajustes

--- app/models/devicesDAO.js
+++ app/models/devicesDAO.js
@@ -1,29 +1,29 @@
 function devicesDAO(conn){
-	this.connection = conn;
+    this.connection = conn;
 }
 
 devicesDAO.prototype.register_devices_db = function(dados, callback) {
-	let query = "insert into user_devices(id_user, device_name, publish, subscribe) values("+dados.id_user+",'"+dados.device_name+"','"+dados.publish+"','"+ dados.subscribe +"')";
-	this.connection.query(query, callback);
+    let query = "insert into user_devices(id_user, device_name, publish, subscribe) values("+dados.id_user+",'"+dados.device_name+"','"+dados.publish+"','"+ dados.subscribe +"')";
+    this.connection.query(query, callback);
 };
 
 devicesDAO.prototype.list_devices_db = function(dados, callback){
-	let query = "SELECT user_devices.*, conn_clients.id_conn, conn_clients.client_id FROM user_devices ";
-	query += "left join conn_clients on conn_clients.device_id = user_devices.device_id ";
-	query += "where user_devices.id_user ="+dados.id_user;
-	this.connection.query(query, callback);
+    let query = "SELECT user_devices.*, conn_clients.id_conn, conn_clients.client_id FROM user_devices ";
+    query += "left join conn_clients on conn_clients.device_id = user_devices.device_id ";
+    query += "where user_devices.id_user ="+dados.id_user;
+    this.connection.query(query, callback);
 };
 
 devicesDAO.prototype.delete_device_db = function(device_id, callback){
-	let query = "DELETE FROM user_devices WHERE device_id =" + device_id;
-	this.connection.query(query, callback);
+    let query = "DELETE FROM user_devices WHERE device_id =" + device_id;
+    this.connection.query(query, callback);
 };
 
 devicesDAO.prototype.connected_device_db = function(dados, callback){
-	let query = "SELECT * FROM conn_clients WHERE id_user="+dados.id_user;
-	this.connection.query(query, callback);
+    let query = "SELECT * FROM conn_clients WHERE id_user="+dados.id_user;
+    this.connection.query(query, callback);
 };
 
 module.exports = function(){
-	return devicesDAO;
+    return devicesDAO;
 };
