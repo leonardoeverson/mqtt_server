e44b2a74addfb5500d3d04b18261078148322077
Leonardo Everson <llinharespinheiro@gmail.com>
2019-01-23 15:18:54 -0300

    refatoração da conexão de dispositivos

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -63,7 +63,7 @@ module.exports.login_dispositivo = function(app, client, username, password, cb)
   	if(typeof(ip) == "undefined"){
   		try{
   			method = "websocket";
-  			ip = client.conn.socket._socket.remoteAddress;
+  			ip = client.conn.socket._socket.remoteAddress.replace("::ffff:","");
   			port = client.conn.socket._socket.remotePort;
   		}catch(e){
   			console.log(e)
@@ -72,76 +72,86 @@ module.exports.login_dispositivo = function(app, client, username, password, cb)
 
   	console.log(method, ip, port);
 
-	loginUsuario.valida_login(dados, function(error, result){
-		if(!error && result.length > 0){
-			bcrypt.compare(password.toString(), result[0].senha, async function (err, res) {
-				if (res === true) {
+  	if(username.search("token") === (-1)){
+        loginUsuario.valida_login(dados, function(error, result){
+            if(!error && result.length > 0){
+                bcrypt.compare(password.toString(), result[0].senha, async function (err, res) {
+                    if (res === true) {
 
-					//informações de conexão do cliente conectado
-					client.conn.remoteIp = ip;
-					client.conn.remotePort = port;
-					client.conn.method_ = method;
-					client.conn.id_user = result[0].id_user;
+                        //informações de conexão do cliente conectado
+                        client.conn.remoteIp = ip;
+                        client.conn.remotePort = port;
+                        client.conn.method_ = method;
+                        client.conn.id_user = result[0].id_user;
 
-					//Finalização de conexão
-					app.app.controllers.connections.db_end_connection(conn);
+                        //Finalização de conexão
+                        app.app.controllers.connections.db_end_connection(conn);
 
-					let result1, result2;
+                        conn_control(app, client, cb, auth_error, result, ip, port);
 
-					try{
-						//Verifica se o dispositivo existe no registro
-						result1 = await app.app.controllers.devices.check_device_reg(app, result[0].id_user, client.id);
+                    } else {
+                        auth_error.returnCode = 4;
+                        cb(auth_error, null)
+                    }
+                })
 
-						//Verifica se é permitida a conexão do dispositivo
-						result2 = await app.app.controllers.settings.get_server_option(app, 1, result[0].id_user);
-					}catch (e) {
-						console.log(e)
-					}
+            }else{
+                if(error){
+                    auth_error.returnCode = 3;
+                    cb(auth_error, null)
 
-					if(result2.length > 0){
-						auth_error.returnCode = 3;
-						cb(auth_error, null);
-						return;
-					}
+                }
 
-					let device_id = result1[0] ? result1[0].device_id : 0;
-					if(typeof (device_id) != "undefined"){
-						client.conn.device_id = device_id;
-					}
+                if(result.length === 0){
+                    auth_error.returnCode = 4;
+                    cb(auth_error, null)
+                }
+            }
+        })
+    }else{
+        console.log("token");
+    }
 
-					try{
-						//controller de conexões
-						let resposta  = await app.app.controllers.connections.conn_mgmt_insert(app, result[0].id_user, client.id, ip, port, device_id);
-
-						//Recuperação de token
-						//app.app.controllers.tokens.token_check(app, request);
-
-						client.conn.conn_id = resposta.insertId;
-					}catch (e) {
-						console.log(e)
-					}
-
-
-					//callback de aceitação da conexão do dispositivo
-					cb(null, true);
-				} else {
-					auth_error.returnCode = 4;
-					cb(auth_error, null)
-				}
-			})
-			
-		}else{
-			if(error){
-				auth_error.returnCode = 3;
-			    cb(auth_error, null)
-
-			}
-
-			if(result.length === 0){
-				auth_error.returnCode = 4;
-			    cb(auth_error, null)
-			}
-		}
-	})
 };
+
+async function conn_control(app, client, cb, auth_error, result, ip, port){
+    let result1, result2;
+
+    try{
+        //Verifica se o dispositivo existe no registro
+        result1 = await app.app.controllers.devices.check_device_reg(app, result[0].id_user, client.id);
+
+        //Verifica se é permitida a conexão do dispositivo
+        result2 = await app.app.controllers.settings.get_server_option(app, 1, result[0].id_user);
+    }catch (e) {
+        console.log(e)
+    }
+
+    if(result2.length > 0){
+        auth_error.returnCode = 3;
+        cb(auth_error, null);
+        return;
+    }
+
+    let device_id = result1[0] ? result1[0].device_id : 0;
+    if(typeof (device_id) != "undefined"){
+        client.conn.device_id = device_id;
+    }
+
+    try{
+        //controller de conexões
+        let resposta  = await app.app.controllers.connections.conn_mgmt_insert(app, result[0].id_user, client.id, ip, port, device_id);
+
+        //Recuperação de token
+        //app.app.controllers.tokens.token_check(app, request);
+
+        client.conn.conn_id = resposta.insertId;
+    }catch (e) {
+        console.log(e)
+    }
+
+
+    //callback de aceitação da conexão do dispositivo
+    cb(null, true);
+}
 
