93d385e5b8a9f4650aff7030c81d8c146f26997f
Leonardo Everson <llinharespinheiro@gmail.com>
2019-01-23 15:55:34 -0300

    ajustes

--- app/controllers/tokens.js
+++ app/controllers/tokens.js
@@ -46,14 +46,13 @@ module.exports.token_check = async function (app, request) {
         let check1 = await token_user_get(app, request, dados);
 
         if (check1.length > 0) {
-            console.log('ch1', check1);
-            request.session.user_token = check1.token_value;
+            return "token:"+check1[0].token_value;
         } else {
             //const namespace = '1b771b54-50e5-991e-68b3-da34ef1f79341';
             dados.uuid = require('uuid/v4')();
             let check2 = await token_user_insert(app, request, dados);
-            console.log('ch2', check2);
-            request.session.user_token = check2.dados.uuid;
+            console.log('ch2', check2[0]);
+            return "token:"+check2[0].dados.uuid;
         }
 
     } catch (e) {
--- app/routes/interfaces.js
+++ app/routes/interfaces.js
@@ -2,7 +2,8 @@ module.exports = function(app){
 
     app.get('/mqtt_web',function(request, response){
         if(request.session.logged){
-            response.render('interface/mqtt_web');
+            console.log(request.session.user_token);
+            response.render('interface/mqtt_web',{token: request.session.user_token});
         }else{
             response.redirect("/");
         }
