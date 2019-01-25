d9be61e195895aa6bf0c60c2cb4ddd01112368ab
Leonardo Everson <llinharespinheiro@gmail.com>
2019-01-25 16:39:06 -0300

    ajustes

--- app/models/utilsDAO.js
+++ app/models/utilsDAO.js
@@ -3,13 +3,13 @@ function utilsDAO(conn){
 }
 
 utilsDAO.prototype.get_prefix_db = function(id_user, callback){
-   let query = "SELECT prefix_value FROM user_prefix WHERE id_user="+id_user;
-   this.conn.query(query, callback);
+   let query = "SELECT prefix_value FROM user_prefix WHERE id_user= ? ";
+   this.conn.query(query, [id_user], callback);
 };
 
 utilsDAO.prototype.insert_prefix_db = function(dados, callback){
     let query = "INSERT INTO user_prefix(id_user, prefix_value) VALUES (?,?)";
-    this.conn.query(query,[dados],callback);
+    this.conn.query(query, [dados], callback);
 };
 
 module.exports = function(){
