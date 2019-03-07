0aad41cbaef0bc761c10cf86eab073b7cb5289cb
Leonardo Everson <llinharespinheiro@gmail.com>
2019-03-07 15:31:46 -0300

    Update dbconn.js

--- config/dbconn.js
+++ config/dbconn.js
@@ -13,7 +13,7 @@ function dbconn(){
 		});
 	}else{
 		return conn = mysql.createConnection({
-			host: 'mysql://'+process.env.MARIADB_SERVICE_HOST+':'+process.env.MARIADB_PORT_3306_TCP_PORT,
+			host: 'mysql://mariadb:'+process.env.MARIADB_PORT_3306_TCP_PORT,
 			user: 'userDAK',
 			password : 'e75WNMWXP0JEkID5',
 			database: 'sampledb',
