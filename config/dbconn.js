f1823bf7016a70a055742242e9c778b3f0a0691c
leonardoeverson <llinharespinheiro@gmail.com>
2019-08-10 10:37:36 -0300

    Update dbconn.js

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,26 +1,26 @@
 let mysql = require('mysql');
 
 function dbconn(){
-	console.log(process.env)
-	if(!process.env.MARIADB_SERVICE_HOST){
+	//console.log(process.env)
+	// if(!process.env.MARIADB_SERVICE_HOST){
+	// 	return conn = mysql.createConnection({
+	// 		host: 'localhost',
+	// 		user: 'root',
+	// 		password : 'db_node_01#',
+	// 		database: 'mqtt',
+	// 		dateStrings : true,
+	// 		multipleStatements: true
+	// 	});
+	//}else{
 		return conn = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password : 'db_node_01#',
-			database: 'mqtt',
-			dateStrings : true,
-			multipleStatements: true
-		});
-	}else{
-		return conn = mysql.createConnection({
-			host: 'mysql://remotemysql.com:3306',
+			host: 'remotemysql.com',
 			user: 'uSANkH0aYN',
 			password : 'gOmWfFmBTa',
 			database: 'uSANkH0aYN',
 			dateStrings : true,
 			multipleStatements: true
 		});
-	}
+	//}
 
 
 }
